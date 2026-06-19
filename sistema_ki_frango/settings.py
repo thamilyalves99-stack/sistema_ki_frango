c31f219efb7c57718c03f34312e3a48a9b5f266b
Thamily Alves <thamilyales99@gmail.com>
2026-06-19 17:36:21 -0300

    adiciona script de inicialização para migração

--- sistema_ki_frango/settings.py
+++ sistema_ki_frango/settings.py
@@ -50,10 +50,9 @@ WSGI_APPLICATION = 'sistema_ki_frango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
