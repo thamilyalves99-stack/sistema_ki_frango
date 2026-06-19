4f53b2ce3bb2089bcb07302327f378f64180a5d4
Thamily Alves <thamilyales99@gmail.com>
2026-06-19 17:20:49 -0300

    cria admin via apps.py

--- Granja_ki_frango/apps.py
+++ Granja_ki_frango/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
 
 class GranjaKiFrangoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'Granja_ki_frango'
+
+    def ready(self):
+        try:
+            User = get_user_model()
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser('admin', 'admin@ki-frango.com', '123456')
+        except OperationalError:
+            pass # A tabela ainda não existe, o migrate vai tratar disso
