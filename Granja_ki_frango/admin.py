c46771160901a1d595feddb2b61d28ca8f17fb8d
Thamily Alves <thamilyales99@gmail.com>
2026-06-19 16:44:28 -0300

    adiciona soma de kg e filtros nos pedidos

--- Granja_ki_frango/admin.py
+++ Granja_ki_frango/admin.py
@@ -1,18 +1,40 @@
 from django.contrib import admin
+from django.db.models import Sum
 from .models import Cliente, Produto, Pedido
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'telefone')
-    search_fields = ('nome',)
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('descricao',)
-    search_fields = ('descricao',)
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
+    # Campos que aparecem na lista
     list_display = ('cliente', 'produto', 'quantidade', 'valor_total', 'entregue', 'data_pedido')
-    list_filter = ('entregue', 'forma_pagamento', 'data_pedido')
+    
+    # Filtros na lateral direita
+    list_filter = ('produto', 'entregue', 'data_pedido')
+    
+    # Campo de busca
     search_fields = ('cliente__nome', 'produto__descricao')
+
+    # Mágica para somar o total de quilos (quantidade)
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context=extra_context)
+        
+        try:
+            # Pega o queryset que já está filtrado na tela
+            qs = response.context_data['cl'].queryset
+            
+            # Soma a 'quantidade' de todos os itens filtrados
+            soma_total = qs.aggregate(total=Sum('quantidade'))['total'] or 0
+            
+            # Adiciona ao contexto para exibir no template
+            response.context_data['total_quantidade'] = soma_total
+        except (AttributeError, KeyError):
+            pass
+            
+        return response
