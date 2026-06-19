4980deeb836664a862532dce018584eebae71275
Thamily Alves <thamilyales99@gmail.com>
2026-06-19 16:57:51 -0300

    força migração no build

--- Granja_ki_frango/admin.py
+++ Granja_ki_frango/admin.py
@@ -1,40 +1,19 @@
 from django.contrib import admin
-from django.db.models import Sum
-from .models import Cliente, Produto, Pedido
-
-@admin.register(Cliente)
-class ClienteAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'telefone')
-
-@admin.register(Produto)
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('descricao',)
+from .models import Pedido, Cliente, Produto
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
-    # Campos que aparecem na lista
     list_display = ('cliente', 'produto', 'quantidade', 'valor_total', 'entregue', 'data_pedido')
-    
-    # Filtros na lateral direita
     list_filter = ('produto', 'entregue', 'data_pedido')
     
-    # Campo de busca
-    search_fields = ('cliente__nome', 'produto__descricao')
+    # Isso faz o funcionário ver apenas os pedidos do dia quando logar
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        # Se for funcionário, você pode filtrar aqui para ele ver apenas o que precisa
+        return qs
 
-    # Mágica para somar o total de quilos (quantidade)
-    def changelist_view(self, request, extra_context=None):
-        response = super().changelist_view(request, extra_context=extra_context)
-        
-        try:
-            # Pega o queryset que já está filtrado na tela
-            qs = response.context_data['cl'].queryset
-            
-            # Soma a 'quantidade' de todos os itens filtrados
-            soma_total = qs.aggregate(total=Sum('quantidade'))['total'] or 0
-            
-            # Adiciona ao contexto para exibir no template
-            response.context_data['total_quantidade'] = soma_total
-        except (AttributeError, KeyError):
-            pass
-            
-        return response
+    # Impede que o funcionário delete pedidos, apenas edite ou adicione
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
--- sistema_ki_frango/settings.py
+++ sistema_ki_frango/settings.py
@@ -176,5 +176,10 @@ JAZZMIN_UI_TWEAKS = {
         "warning": "btn-warning",
         "danger": "btn-danger",
         "success": "btn-success"
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   
     }
 
