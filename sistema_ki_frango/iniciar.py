42f86a05ce456883cc14becd559e4c2474019dd4
Thamily Alves <thamilyales99@gmail.com>
2026-06-19 17:08:37 -0300

    corrige estrutura do settings.py

--- sistema_ki_frango/iniciar.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-from django.core.management import call_command
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_ki_frango.settings')
-django.setup()
-call_command('migrate')
-print("Banco de dados criado com sucesso!")
